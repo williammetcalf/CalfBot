f17918616af78501cb20baa4ef304abbf27b72f8
William Metcalf <metcalf_william@network.lilly.com>
2018-11-26 13:57:59 -0500

    Initial Commit

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,31 +1,46 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const outputDirectory = 'dist';
 
 module.exports = {
-  entry: "./main.js",
+  entry: ['babel-polyfill', './src/client/index.js'],
   output: {
-    path: path.join(__dirname, "/bundle"),
-    filename: "index_bundle.js"
-  },
-  devServer: {
-    inline: true,
-    port: 8080
+    path: path.join(__dirname, outputDirectory),
+    filename: 'bundle.js'
   },
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
+        test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
-        query: {
-          presets: ["@babel/preset-env", "@babel/preset-react"]
+        use: {
+          loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
       }
     ]
   },
+  devServer: {
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api': 'http://localhost:8080'
+    }
+  },
   plugins: [
+    new CleanWebpackPlugin([outputDirectory]),
     new HtmlWebpackPlugin({
-      template: "./index.html"
+      template: './public/index.html',
+      favicon: './public/favicon.ico'
     })
   ]
 };
